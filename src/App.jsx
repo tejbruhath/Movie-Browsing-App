ff629c2e4ae7cd00297337544d584d0d7a745a49
tejbruhath <tejbruhath001@gmail.com>
2025-10-19 16:52:09 +0530

    boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export default function App(){
+  return(
+    <main>
+    <div className="pattern"></div>
+    <div className="wrapper">
+      <h1>Find <span className="text-gradient">Movies</span> that you enjoy !</h1>
+    </div>
+    </main>
+  )
+}
